4b554829831cb4afb4a9d4f3012306ccc5838de8
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-10-07 17:50:23 +0200

    fixes

--- src/cli/setup_option_parser.py
+++ src/cli/setup_option_parser.py
@@ -6,25 +6,34 @@ def setup_option_parser(argv):
     parser.add_argument('-h', '--help',
                         action='help',
                         help='Display this message.')
-    parser.add_argument('--token',
-                        dest='GITHUB_TOKEN',
+    parser.add_argument('-t', '--token',
+                        metavar='',
                         help='GitHub developer token.')
-    parser.add_argument('--gom',
+    parser.add_argument('-gom',
+                        metavar='',
                         help='List organization members.')
-    parser.add_argument('--gomu',
+    parser.add_argument('-gomu',
+                        metavar='',
                         help='Check organization membership for a user.')
-    parser.add_argument('--grc',
+    parser.add_argument('-grc',
+                        metavar='',
                         help='List repository collaborators.')
-    parser.add_argument('--grcu',
+    parser.add_argument('-grcu',
+                        metavar='',
                         help='Check if a user is a repository collaborator.')
-    parser.add_argument('--cpr',
+    parser.add_argument('-cpr',
+                        metavar='',
                         help='Draft pull requests are available in public repositories.')
-    parser.add_argument('--upr',
+    parser.add_argument('-upr',
+                        metavar='',
                         help='Draft pull requests are available in public repositories.')
-    parser.add_argument('--mpr',
+    parser.add_argument('-mpr',
+                        metavar='',
                         help='Merge a pull request.')
-    parser.add_argument('--crepo',
+    parser.add_argument('-crepo',
+                        metavar='',
                         help='Create a new repository for the authenticated user.')
-    parser.add_argument('--drepo',
+    parser.add_argument('-drepo',
+                        metavar='',
                         help='Deleting a repository')
     return parser.parse_args(argv)
